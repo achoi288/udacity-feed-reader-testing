f7b90d7fe400637974b7ffa6c8bf35e6b82b7be4
achoi288 <albert.choi288@gmail.com>
2018-09-20 14:02:38 -0500

    test: Add Tests For Initial Entries And New Feed Selection
    1. Load index.html in a Chrome browser.
    
    2. Scroll to the bottom of the page.
    
       -> The Jasmine summary shows that all tests in the "RSS Feeds", "The menu",  "Initial Entries", and "New Feed Selection" test suites pass.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -80,19 +80,59 @@ $(function () {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* A new test suite named "Initial Entries" */
+    describe('Initial Entries', function () {
+        /* A test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        const feedElement = document.getElementsByClassName('feed')[0];
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+        beforeEach(function (done) {
+            expect(feedElement.getElementsByClassName('entry').length === 0).toBe(true);
+            loadFeed(0, function () {
+                done();
+            });
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        it('have at least one feed entry', function () {
+            expect(feedElement.getElementsByClassName('entry').length > 0).toBe(true);
+        });
+    });
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
+    /* A new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function () {
+        /* A test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         * We will load the first feed, examine the 'header-title' element,
+         * record the first feed entries InnerText, load the third feed, examine the 
+         * 'header-title' element, and compare the InnerText of the first feed entry
+         * with that from the first feed loaded.
+         */
+        const headerTitleElement = document.getElementsByClassName('header-title')[0];
+        const feedElement = document.getElementsByClassName('feed')[0];
+        let firstFeedEntryInnerText;
+
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                expect(headerTitleElement.textContent === allFeeds[0].name).toBe(true);
+                expect(headerTitleElement.textContent === allFeeds[2].name).toBe(false);
+                firstFeedEntryInnerText = feedElement.firstElementChild.innerText
+                expect(firstFeedEntryInnerText.length > 0).toBe(true);
+
+                loadFeed(2, function () {
+                    done();
+                });
+        });
+
+        it('changes content', function () {
+            expect(headerTitleElement.textContent === allFeeds[2].name).toBe(true);
+            const firstFeedEntryInnerTextAfterNewFeedSelection = feedElement.firstElementChild.innerText;
+            expect(firstFeedEntryInnerTextAfterNewFeedSelection.length > 0).toBe(true);
+            expect(firstFeedEntryInnerText === firstFeedEntryInnerTextAfterNewFeedSelection).toBe(false);
+        });
+    });
 }());
